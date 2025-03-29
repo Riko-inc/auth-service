d57714025c93adc438adb66cfb0c9567b9055c89
Handlest <91721625+Handlest@users.noreply.github.com>
2025-03-29 21:04:02 +0700

    Feature/dev 29 (#2)
    * test swagger openapi definition server url
    
    
    ---------
    
    Co-authored-by: Maxim <therikomero@gmail.com>

--- src/main/java/org/example/authservice/config/OpenApiConfig.java
+++ src/main/java/org/example/authservice/config/OpenApiConfig.java
@@ -5,8 +5,12 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 
-@OpenAPIDefinition (info = @Info(
+@OpenAPIDefinition (
+        servers = {
+                @Server(url = "http://localhost:${server.port}${SWAGGER_PREFIX:}", description = "Server")},
+        info = @Info(
         title = "Authentication module of task management platform",
         description = "Made as research project at TPU", version = "1.0.0",
         contact = @Contact(
