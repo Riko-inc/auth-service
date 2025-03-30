3055e54bf4fe8a38200c021eda7c8b77cc8c89ac
Maxim <therikomero@gmail.com>
2025-03-31 01:02:31 +0700

    fix for whitelisted urls

--- src/main/java/org/example/authservice/config/WebSecurityConfig.java
+++ src/main/java/org/example/authservice/config/WebSecurityConfig.java
@@ -28,7 +28,11 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 public class WebSecurityConfig {
 
     public static final String[] WHITE_LIST_URL = {
-            "/api/v1/auth/**",
+            "/api/v1/auth/register",
+            "/api/v1/auth/refresh-token",
+            "/api/v1/auth/authenticate",
+            "/api/v1/auth/check-token",
+            "/api/v1/auth/check-email",
             "/v2/api-docs",
             "/v3/api-docs",
             "/v3/api-docs/**",
