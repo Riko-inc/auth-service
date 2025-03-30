a499d6775b7c6b9d82558ae9484ea33f3356bf13
Handlest <91721625+Handlest@users.noreply.github.com>
2025-03-30 23:04:26 +0700

    Feature/dev 29 (#4)
    * fix docker configuration and add variable parameters
    
    * tmp test
    
    ---------
    
    Co-authored-by: Maxim <therikomero@gmail.com>

--- src/main/java/org/example/authservice/config/OpenApiConfig.java
+++ src/main/java/org/example/authservice/config/OpenApiConfig.java
@@ -9,7 +9,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition (
         servers = {
-                @Server(url = "http://${SERVER_IP:localhost}:${SERVER_HTTP_PORT:server.port}${SWAGGER_PREFIX:}", description = "Server")},
+                @Server(url = "http://${SERVER_IP:localhost}:${SERVER_HTTP_PORT:8083}${SWAGGER_PREFIX:}", description = "Server")},
         info = @Info(
         title = "Authentication module of task management platform",
         description = "Made as research project at TPU", version = "1.0.0",
