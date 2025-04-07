64a99c5b727cd2a7348b967ae293ce94f969b99c
Maxim <therikomero@gmail.com>
2025-04-07 23:56:59 +0700

    fix for mapper

--- src/main/java/org/example/authservice/domain/dto/responses/GetUserResponse.java
+++ src/main/java/org/example/authservice/domain/dto/responses/GetUserResponse.java
@@ -3,15 +3,18 @@ package org.example.authservice.domain.dto.responses;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.example.authservice.domain.entities.UserEntity;
 
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Schema(description = "Ответ на получение пользователя")
-@Builder
 public class GetUserResponse {
     @Schema(description = "Id пользователя", example = "12")
     private Long id;
